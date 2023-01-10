292fd8435b40537fa4afe3bd168d8199fcd2ffaf
Bernhard Haubold <haubold@evolbio.mpg.de>
2023-01-10 11:40:46 +0100

    Added child array

--- esa.go
+++ esa.go
@@ -13,6 +13,23 @@ import (
 	"unsafe"
 )
 
+type Stack []*Interval
+type Interval struct {
+	Idx int
+	Lcp int
+}
+
+func (s *Stack) Top() *Interval {
+	return (*s)[len(*s)-1]
+}
+func (s *Stack) Pop() *Interval {
+	i := (*s)[len(*s)-1]
+	(*s) = (*s)[0 : len(*s)-1]
+	return i
+}
+func (s *Stack) Push(i *Interval) {
+	(*s) = append(*s, i)
+}
 func Sa(t []byte) []int {
 	var sa []int
 	header := (*reflect.SliceHeader)(unsafe.Pointer(&t))
@@ -55,4 +72,42 @@ func Lcp(t []byte, sa []int) []int {
 		}
 	}
 	return lcp
+}
+func Cld(lcp []int) []int {
+	var cld []int
+	lcp = append(lcp, -1)
+	n := len(lcp) - 1
+	cld = make([]int, n+1)
+	cld[0] = n
+	stack := new(Stack)
+	iv := newInterval(0, -1)
+	stack.Push(iv)
+	for i := 1; i <= n; i++ {
+		top := stack.Top()
+		for lcp[i] < top.Lcp {
+			last := stack.Pop()
+			top = stack.Top()
+			for top.Lcp == last.Lcp {
+				cld[top.Idx] = last.Idx
+				last = stack.Pop()
+				top = stack.Top()
+			}
+			top = stack.Top()
+			if lcp[i] < top.Lcp {
+				cld[top.Idx] = last.Idx
+			} else {
+				cld[i-1] = last.Idx
+			}
+		}
+		iv = newInterval(i, lcp[i])
+		stack.Push(iv)
+	}
+	lcp = lcp[:len(lcp)-1]
+	return cld
+}
+func newInterval(i, l int) *Interval {
+	iv := new(Interval)
+	iv.Idx = i
+	iv.Lcp = l
+	return iv
 }
